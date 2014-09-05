37b4dac98cd57a289da69986a6d2474d0d2149cf
Red4x <ryda.arush@gmail.com>
2014-09-05 19:20:39 +0800

    minor update
    minor update

--- js/screen.js
+++ js/screen.js
@@ -1,15 +1,14 @@
 
 // Animation ryda//
 var wss_i = 0;
-			var wss_array =["coffee addicted","<u>pixel pusher</u>","119.81 .31.34","ryda.com"];
+			var wss_array =["coffee addicted","<u>pixel pusher</u>","health-conscious","ryda.com"];
 			var wss_elem;
 
 			function wssNext(){
 				wss_i++;
 				wss_elem.style.opacity=0;
 				if(wss_i > (wss_array.length - 1)){
-					wss_i }
-					 //window.location.href = 'http://www.iamryda.com/';}
+					window.location.href = 'http://www.iamryda.com/en/';				}
 				setTimeout('wssSlide()',1000);
 			}
 
